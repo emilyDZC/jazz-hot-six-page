d848bdadfde42ef4910eb6d6c97dc27dcee65d48
Emily Chaplais <emilychaplais@hotmail.com>
2023-03-24 15:05:55 +0000

    Create gig list page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="homepage">
+        <img src="../assets/jazz-hot-six.jpeg" width="500"/>
+        <h1>Gig List</h1>
+
+        <div>Saturday 25th March - Trippets</div>
+        <div>Tuesday 4th April - Dorothy Pax</div>
+        <div>Saturday 22nd April - Trippets</div>
+        <div>Saturday 29th April - Sharrow Cemetery</div>
+        <div>Tuesday 2nd May - Dorothy Pax</div>
+        <div>Saturday 6th May - Balboa dancers</div>
+        <div>Tuesday 6th June - Dorothy Pax</div>
+        <div>Tuesday 4th July - Dorothy Pax</div>
+        <div>Sunday 23rd July - Pax in the Park (Tramlines Weekend) - afternoon</div>
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld'
+}
+</script>
+
+<style>
+    .homepage {
+        font-weight: 700;
+    }
+</style>
