f092a0a892441114db8b0975fa4ba398d8341cbc
Emily Chaplais <emilychaplais@hotmail.com>
2023-08-10 11:29:41 +0100

    Add pics and update gigs

---
+++ src/components/EventRow.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="event-row">
+        <s v-if="past">
+            {{ date }} - {{ venue }} ({{ players }})
+        </s>
+        <template v-else>
+            {{ date }} - {{ venue }} ({{ players }})
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EventRow',
+    props: {
+        past: Boolean,
+        date: String,
+        venue: String,
+        players: String
+    }
+}
+</script>
+
+<style scoped>
+    .event-row {
+        color: bisque;
+    }
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="homepage">
-        <img src="../assets/jazz-hot-six.jpeg" width="500"/>
-        <h1>Gig List</h1>
+        <div class="left">
+            <img class="pic" src="../assets/martin1.jpeg" width="200" />
+            <img class="pic" src="../assets/emily1.jpeg" width="200" />
+            <img class="pic" src="../assets/jazz-hot-six-1.jpeg" width="400" />
+        </div>
+        <div class="middle">
+            <img src="../assets/jazz-hot-six.jpeg" width="300"/>
+            <h1 class="title">Gig List</h1>
 
-        <div><s>Saturday 29th April - Sharrow Cemetery (all)</s></div>
-        <div><s>Tuesday 2nd May - Dorothy Pax (all)</s></div>
-        <div><s>Saturday 6th May - Balboa dancers (no Emily)</s></div>
-        <div>Wednesday 17th May - Big Swing (all)</div>
-        <div>Friday 19th May - Trippets (no Andrew or Kate. Mike Fleming on Bass)</div>
-        <div>Sunday 28th May - Leopold Square (no Kate or Emily. Maybe Steve Brickley - TBC)</div>
-        <div>Saturday 3rd June - Trippets (no Emily)</div>
-        <div>Tuesday 6th June - Dorothy Pax (no Shez)</div>
-        <div>Tuesday 4th July - Dorothy Pax</div>
-        <div>Sunday 23rd July - Pax in the Park (Tramlines Weekend) - afternoon (no Shez. John England on guitar)</div>
+            <EventRow v-for="event in events" :date="event.date" :venue="event.venue" :players="event.players" :past="event.past" :key="event.date"/>
+        </div>
+        <div class="right">
+            <img class="pic" src="../assets/kate1.jpeg" width="200" />
+            <img class="pic" src="../assets/andrew1.jpeg" width="200" />
+        </div>
 
         <br/>
         <br/>
@@ -22,17 +24,79 @@
 </template>
 
 <script>
+import EventRow from "./EventRow.vue"
+
 export default {
-    name: 'HelloWorld'
+    name: 'HelloWorld',
+    components: { EventRow },
+    data() {
+        return {
+            events: [
+                {
+                    date: "Tuesday 6th June",
+                    venue: "Dorothy Pax",
+                    players: "no Shez",
+                    past: true
+                },
+                {
+                    date: "Tuesday 4th July",
+                    venue: "Dorothy Pax",
+                    players: "all",
+                    past: true
+                },
+                {
+                    date: "Sunday 23rd July",
+                    venue: "Pax in the Park (Tramlines Weekend) - afternoon",
+                    players: "all",
+                    past: true
+                },
+                {
+                    date: "Tuesday 5th September",
+                    venue: "Dorothy Pax",
+                    players: "all",
+                    past: false
+                },
+                {
+                    date: "Saturday 7th October",
+                    venue: "TBC",
+                    players: "all",
+                    past: false
+                },
+            ]
+        }
+    }
 }
 </script>
 
 <style>
+    body {
+        background-color: rgb(76, 90, 104);
+    }
+
     .homepage {
         font-weight: 700;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .title {
+        color: rgb(250, 174, 51);
+    }
+
+    .left, .right {
+        width: 30vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 
     img {
         max-width: 100%;
+    }
+
+    .pic {
+        border: 5px solid beige;
+        margin: 5px;
     }
 </style>
